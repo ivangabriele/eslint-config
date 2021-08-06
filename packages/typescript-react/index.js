8cbfe46e4eb65088094d7739569163a5e63e4e2a
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2021-08-06 12:14:30 +0200

    feat: enforce imports and keys sorting rules (#8)

--- packages/typescript-react/index.js
+++ packages/typescript-react/index.js
@@ -2,6 +2,23 @@ module.exports = {
   extends: ['airbnb-typescript', 'prettier'],
   plugins: ['prettier'],
   rules: {
+    'sort-keys': 'error',
+
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        groups: [
+          ['builtin', 'external'],
+          ['index', 'internal', 'parent', 'sibling'],
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+
     'prettier/prettier': 'error',
   },
 }
