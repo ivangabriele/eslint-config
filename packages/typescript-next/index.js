9b08a05c315c880f51f1e37a53c771d91aec995a
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-08-23 19:01:55 +0200

    feat: disable react/require-default-props rule

--- packages/typescript-next/index.js
+++ packages/typescript-next/index.js
@@ -29,6 +29,7 @@ module.exports = {
     'react/jsx-sort-props': 'error',
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
 
     'sort-destructure-keys/sort-destructure-keys': ['error', { caseSensitive: false }],
 
